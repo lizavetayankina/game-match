d699d6a7b745d4f7223a8acf9573e98a4a70bb34
Lizaveta Yankina <lizavetayankina@gmail.com>
2021-02-17 13:53:38 +0300

    add router but it isn't work

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
-
-import Content from './Content';
-import Modal from './Modal';
-import Board from './Board';
 import '../styles/Header.css';
+import Modal from './Modal';
+//import HeaderGame from './HeaderGame';
+//import Board from './Board';
+//import { Route, Router, Switch } from 'react-router-dom';
+//import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 
 export default function Header () {
 
 const [modal, setModal] = useState({modal1:false}) 
-const [board, setBoard] = useState({board:false})
+
 
 return (
 <div className="nav">
@@ -22,11 +23,17 @@ return (
 <a href="#" className="nav-link" onClick={() => setModal({...modal,modal1:true})} >Log in </a>
 </div>
 
-
 <Modal isOpened={modal.modal1} 
 onModalClose={() => setModal({...modal, modal1:false})} />
 
+
+{/*<Router>
+  <Switch>
+    <Route exact path="/" component={HeaderGame} />
+  </Switch>
+</Router>  */}
 </div>
+
 
 );
 }
