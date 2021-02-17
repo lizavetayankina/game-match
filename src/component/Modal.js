901c5fc0ffc564024fde3adecafec6e9992cc135
lizavetayankina <lizavetayankina@gmail.com>
2021-02-17 22:00:17 +0300

    i try get value from modal

--- src/component/Modal.js
+++ src/component/Modal.js
@@ -4,9 +4,30 @@ import '../styles/Modal.css';
 
 const Modal = props => {
 
+let userInfo = [];
+let firstName = React.createRef();
+let lastName = React.createRef();
+
     function f1() {
-        console.log ('ggggg');
+    console.log ('ggggg');
     }
+    
+    function showInput(event) {
+        const first = firstName.current.value;
+        console.log(first);
+        const last = lastName.current.value;
+        console.log(last);
+
+        let inpOneRest = {first: first,
+        inp: 1}
+        let inpTwoRest = { last: last, 
+        inp: 2}
+        userInfo.push(inpOneRest);
+        userInfo.push(inpTwoRest);
+        console.log(userInfo);
+    }
+
+ 
 
     return (
         <div className={`modal_wrapper ${props.isOpened ? 'open' : 'close'}`} >
@@ -14,12 +35,12 @@ const Modal = props => {
             <div className='modal_close' onClick={props.onModalClose}></div>
             <form>
             <label>First name</label>
-            <input type='name' className='form-control'></input>
+            <input type='name' className='form-control firstName' onInput={showInput} ref={firstName}/>
             <label>Last name</label>
-            <input type='name' className='form-control'></input>
+            <input type='name' className='form-control lastName'onInput={showInput} ref={lastName}/>
             <label>Email</label>
-            <input type='email' className='form-control'></input>
-            <button className="btn" onClick={f1}> Submit </button>
+            <input type='email' className='form-control'/>
+            <button className="btn" onClick={f1}>Submit </button>
             <button className="btn" onClick={props.onModalClose}>x</button>
             </form>
         </div>
